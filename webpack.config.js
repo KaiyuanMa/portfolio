43c2c020ef885996d1be57c97689fb02bcfc79a3
Kaiyuan Ma <frankmkmk1@gmail.com>
2022-11-20 17:03:49 -0500

    added html

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,14 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
 module.exports = {
   devtool: "source-map",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "index_bundle.js",
+  },
+  plugins: [new HtmlWebpackPlugin()],
   module: {
     rules: [
       {
