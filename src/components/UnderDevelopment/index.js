7c4e197e912572ee34a42f8fa724f40cf3173df5
Kaiyuan Ma <frankmkmk1@gmail.com>
2023-03-14 17:58:16 -0400

    added underdevelopment

---
+++ src/components/UnderDevelopment/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./styles.css";
+
+const index = () => {
+  return (
+    <div className="under-development">
+      <h1>Under Development</h1>
+      <p>
+        The mobile version of this website is currently under development.
+        Please visit us from a larger screen.
+      </p>
+    </div>
+  );
+};
+
+export default index;
--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import { Route, Routes, NavLink, HashRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 const root = createRoot(document.querySelector("#root"));
+import UnderDevelopment from "./components/UnderDevelopment";
 
 root.render(
   <HashRouter>
+    <UnderDevelopment />
     <App />
   </HashRouter>
 );
